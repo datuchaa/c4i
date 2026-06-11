7998cd51f14866483d87f83300264737ea5457ba
datuchaa <abramishvili98@gmail.com>
2026-06-11 17:18:22 +0400

    refactor ESLint configuration for React to include ignore patterns and streamline TypeScript integration

---
+++ .changeset/fair-heads-train.md
@@ -0,0 +1,6 @@
+---
+"c4i-eslint-react": minor
+"c4i-eslint-node": minor
+---
+
+Refactor ESLint configuration for React to include ignore patterns and streamline TypeScript integration
--- packages/eslint-config-react/eslint-config-react.js
+++ packages/eslint-config-react/eslint-config-react.js
@@ -3,13 +3,23 @@ import eslintConfigPrettier from 'eslint-config-prettier';
 import pluginReact from 'eslint-plugin-react';
 import pluginReactHooks from 'eslint-plugin-react-hooks';
 import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 /**
  *
  * @type {import("eslint").Linter.Config}
  * */
 export const config = [
+  {
+    ignores: [
+      '**/dist/**',
+      '**/dist-ssr/**',
+      '**/node_modules/**',
+      '**/coverage/**',
+    ],
+  },
   js.configs.recommended,
+  tseslint.configs.base,
   eslintConfigPrettier,
   pluginReact.configs.flat['jsx-runtime'],
   {
